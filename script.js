fcd881be09ab480adecba9cfd99af377ae1977d0
Anjalivlog <139326744+Anjalivlog@users.noreply.github.com>
2024-08-05 16:45:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const parseCode = (str) => {
   // your code here
-};
+	let parts = str.split('0');
+	const filteredParts = parts.filter(part => part !== '');
+	const [firstName, lastName, id] = filteredParts;
+	return {
+		firstName: firstName,
+		lastName: lastName,
+		id: id
+	}
+}; 
 
 // Do not change the code below
 const str = prompt("Enter str: ");
